bcc829d25906c623f0f2f39be7e2d0bc43e8095a
Vlad <suzirie@gmail.com>
2017-09-03 19:01:12 +0300

    before cities add

---
+++ data/cities.js
@@ -0,0 +1,49 @@
+// var cities = [
+//     {name:'Вінниця', area:'', region:'Вінницька'},
+//     {name:'Ладижин', area:'', region:'Вінницька'},
+//     {name:'Володимир-Волинський', area:'', region:'Волинська'},
+//     {name:'Нововолинський', area:'', region:'Волинська'},
+//     {name:'Вільногірськ', area:'', region:''},
+//     {name:'Жовтоводськ', area:'', region:''},
+//     {name:'Марганець', area:'', region:''},
+//     {name:'Орджонікідзе', area:'', region:''},
+//     {name:'Першотравенськ', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//     {name:'', area:'', region:''},
+//
+// ];
+//
+// "12:";
+// obl3_4[1] = "13:";
+// obl3_4[2] = "14:Марганець";
+// obl3_4[3] = "15:Орджонікідзе";
+// obl3_4[4] = "16:Першотравенськ";
+// obl3_4[5] = "17:Тернівка";
